78620f946a9b1db7a0f6613ed09425682321f1cc
SimonXu <simon.xu72@gmail.com>
2017-02-22 16:32:20 +0800

    initial version

---
+++ datetime_x_fin.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author Simon Xu
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc Provide string datetime manipulation functions
+%%%
+%%% @end
+%%% Created : 22. Feb 2017 4:20 PM
+%%%-------------------------------------------------------------------
+-module(datetime_x_fin).
+-author("simon").
+
+%% API
+-export([
+
+]).
